efa4fd07caa70ae40800f5f4cdbf54496071a952
Animeology <joseph.nguyen19@yahoo.com>
2023-06-11 16:34:36 -0500

    Update Anime List.cs
    Revised the menu to loop until done
    Finished with WatchedList()

--- Anime List.cs
+++ Anime List.cs
@@ -1,4 +1,7 @@
-﻿public class AnimeList
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+public class AnimeList
 {
     public static void Main(string[] args)
     {
@@ -8,39 +11,63 @@
 
     static void Menu()
     {
-        Console.WriteLine("1. WatchList");
-        Console.WriteLine("2. Done/Watched");
-        Console.WriteLine("3. Dropped");
-        Console.WriteLine("4. Quit");
+        int choice = 0;
 
-        int choice = Convert.ToInt32(Console.ReadLine());
-
-        switch (choice)
+        while (choice != 4)
         {
-            case 1:
-                WatchList();
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            default:
-                Console.WriteLine("Invalid choice");
-                Menu();
-                break;
+            Console.WriteLine("1. WatchList");
+            Console.WriteLine("2. Done/Watched");
+            Console.WriteLine("3. Dropped");
+            Console.WriteLine("4. Quit");
+
+            choice = Convert.ToInt32(Console.ReadLine());
+
+
+            if (choice != 4)
+            {
+                Console.WriteLine("Type the anime");
+                string anime = Console.ReadLine()!;
+
+                string file = "WatchList.txt";
+
+                switch (choice)
+                {
+                    case 1:
+                        WatchList(anime, file);
+                        break;
+                    case 2:
+                        WatchedList(anime, file);
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        Menu();
+                        break;
+                }
+            }
         }
     }
 
-    static void WatchList()
+    static void WatchList(string anime, string file)
     {
-        Console.WriteLine("Type the anime you want to watch");
-        string anime = Console.ReadLine()!;
-
-        string file = "WatchList.txt";
         File.AppendAllText(file, anime + Environment.NewLine);
     }
 
+    static void WatchedList(string anime, string watchFile)
+    {
+        string doneFile = "DoneList.txt";
 
+        Regex regex = new Regex(anime);
+
+        using (StreamWriter sw = new StreamWriter(watchFile))
+        {
+            string content = regex.Replace(anime, string.Empty);
+            sw.Write(content);
+        }
+
+        File.AppendAllText(doneFile, anime + Environment.NewLine);
+    }
 }
